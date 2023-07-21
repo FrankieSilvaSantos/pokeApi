4b9d5f275328b32fde101b8186af3aa7872ec163
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-07-21 19:56:38 -0300

    project

---
+++ app/header/page.js
@@ -0,0 +1,33 @@
+import styles from './Header.module.css'
+import Link from 'next/link'
+
+
+export default function Header() {
+
+    return(
+
+        <>
+            <nav className={`navbar navbar-expand-lg  ${styles.navStyle}`}>
+  <div className="container-fluid">
+    <a className={`navbar-brand ${styles.logoStyle}`} href="#"><img className={`${styles.imgHeader}`} src="/img/logo/pokeball.png"></img> PokeApi</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className={`collapse navbar-collapse ${styles.listPositioning}`} id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className={`nav-link  ${styles.linkStyle}`} aria-current="page" href="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className={`nav-link  ${styles.linkStyle}`} href="#">About</Link>
+        </li>
+      
+      </ul>
+    </div>
+  </div>
+</nav>
+        </>
+
+    )
+
+}
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Header from './header/page'
+import Footer from './footer/page'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,51 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+
+      {/* BOOTSTRAP */}
+
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossOrigin="anonymous"></link>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossOrigin="anonymous"></script>
+
+      {/* MDB */}
+
+      
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+  rel="stylesheet" crossOrigin="anonymous"
+/>
+
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet" crossOrigin="anonymous"
+/>
+
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css"
+  rel="stylesheet" crossOrigin="anonymous"
+/>
+
+
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/#[[latestVersion]]#/mdb.min.js"
+  crossOrigin="anonymous"
+></script>  
+
+   
+
+
+      </head>
+      <body className={inter.className}>
+      
+      <header><Header></Header></header>
+      
+      <main>{children}</main>
+      
+        <footer><Footer></Footer></footer>
+      
+      </body>
     </html>
   )
 }
