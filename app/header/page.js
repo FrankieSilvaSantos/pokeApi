5910ffe8ff2966b5fc745cd8c0929fc46b21a853
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-07-21 20:04:19 -0300

    togglerStyle

--- app/header/page.js
+++ app/header/page.js
@@ -10,8 +10,8 @@ export default function Header() {
             <nav className={`navbar navbar-expand-lg  ${styles.navStyle}`}>
   <div className="container-fluid">
     <a className={`navbar-brand ${styles.logoStyle}`} href="#"><img className={`${styles.imgHeader}`} src="/img/logo/pokeball.png"></img> PokeApi</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+    <button className={`navbar-toggler ${styles.togglerStyle}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className={`navbar-toggler-icon ${styles.togglerStyle}`}></span>
     </button>
     <div className={`collapse navbar-collapse ${styles.listPositioning}`} id="navbarNav">
       <ul className="navbar-nav">
