3d6d7b114a71f3a9e725e73dd1519c32eebe0e51
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-07-21 22:03:51 -0300

    stylesHeaderHome

--- app/page.js
+++ app/page.js
@@ -4,9 +4,12 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={`${styles.containerHome}`}>
+      <section className={`${styles.containerTituloHome}`}>
         <h1 className={`${styles.titleHome}`}><span className={`${styles.spanHome}`}>Poke</span>Api <img className={`${styles.imgHome}`} src='/img/logo/pokeball.png'></img></h1>
+        
+
       <section className={`${styles.borderHome}`}></section>
-   
+      </section>
     </main>
   )
 }
