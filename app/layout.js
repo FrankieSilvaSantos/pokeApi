14d02a5971868f84fefbc4c23bf3fea6b0c18ba9
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-07-23 00:44:02 -0300

    favicon

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <link rel="icon" href="/img/logo/pokeApi.ico" sizes="any" />
 
       {/* BOOTSTRAP */}
 
