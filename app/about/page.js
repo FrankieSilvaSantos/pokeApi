4c20564df32c79301ce28d4ba68c476c4806a0d6
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-07-27 19:22:23 -0300

    perhapsResponsive

--- app/about/page.js
+++ app/about/page.js
@@ -12,33 +12,39 @@ export default function About() {
         
 
         <section>
-        <img className={`${styles.imgAbout}`} src="/img/about/about.jpg"></img>
+        <img className={`${styles.imgAbout}`} src="/img/about/about2.jpg"></img>
+        <section className={`${styles.borderLeftAbou3}`}></section>
         </section>
         
     
-
+     
  
-
+      
      
        
    
 
-            <section style={{position:'relative',width:'250%'}}>
+
+          <section style={{position:'relative'}}>
           
-           
             <section className={`${styles.textAbout}`}>
             <section className={`${styles.borderLeftAbout}`}></section>
-            <section className={`${styles.borderLeftAbou2}`}></section>
+          
+           
+           
         <h1 className={`${styles.titleAbout}`}>Sobre o <span className={`${styles.spanAbout}`}>Projeto</span></h1>
-
+        <section className={`${styles.borderLeftAbou2}`}></section>
         <p className={`${styles.pAbout}`}>“All Pokemon And People Were  Born Because  They Have A Part  In The World.”</p>
         </section>
         </section>
-        
-        
+
+   
+
         </section>
         
+       
         </section>
+        
 
     )
 
